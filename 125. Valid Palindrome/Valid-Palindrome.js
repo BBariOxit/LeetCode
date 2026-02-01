cc471587f5c963bfc4a57741e8a0dc838bc84b02
phanthaibao <phanbao2648@gmail.com>
2026-02-01 19:29:56 +0700

    Valid-Palindrome

--- 125. Valid Palindrome/Valid-Palindrome.js
+++ 125. Valid Palindrome/Valid-Palindrome.js
@@ -23,3 +23,33 @@
 // Constraints:
 // 1 <= s.length <= 2 * 105
 // s consists only of printable ASCII characters.
+
+//cách 1: Chaining Methods
+// Logic:
+// Dùng Regex để dọn rác và hạ lùn chuỗi.
+// Biến chuỗi thành mảng -> Đảo ngược mảng -> Dán lại thành chuỗi. -> So sánh.
+// Time: O(n)
+// Space: O(n) - Tệ nhất là nó tạo ra 3-4 cái bản sao của chuỗi trong bộ nhớ.
+const isPalindrome = (s) => {
+  const clean_s = s.toLowerCase().replace(/[^a-z0-9]/g, '')
+  return clean_s === clean_s.split('').reverse().join('') // tốn ram kinh 
+}
+
+// cách 2: duyệt một nửa chuỗi
+// Logic:
+// Dọn rác bằng Regex trước.
+// Dùng vòng lặp chạy đến Length / 2.
+// So sánh ký tự thứ i với ký tự thứ length - 1 - i.
+const isPalindrome2 = (s) => {
+  const clean_s = s.toLowerCase().replace(/[^a-z0-9]/g, '')
+  const len = clean_s.length 
+  for (let i = 0; i < (len / 2); i++) {
+    if (clean_s[i] !== clean_s[len -1 -i]) {
+      return false
+    }
+  } return true
+}
+//chạy
+const s = " "
+console.log(isPalindrome(s))
+console.log(isPalindrome2(s))
